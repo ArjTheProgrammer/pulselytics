e2293e0031c774506096c0a12cfe10a6fdfe2784
Jose Neil Silagan Jr. <neilsilagan@gmail.com>
2024-10-06 01:03:23 +0800

    Add remedies for the switch scenes

--- src/main/java/application/pulselytics/controllers/HelloController.java
+++ src/main/java/application/pulselytics/controllers/HelloController.java
@@ -1,14 +1,37 @@
 package application.pulselytics.controllers;
 
+import java.io.IOException;
+import java.util.Objects;
+
+import application.pulselytics.HelloApplication;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void switchToSignIn(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(HelloApplication.class.getResource("scenes/SignIn.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void switchToSignUp(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(HelloApplication.class.getResource("scenes/SignUp.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
