2d35f5fd8e8ec483f98d90ceb0ece4522f89c9b8
Jose Neil Silagan Jr. <neilsilagan@gmail.com>
2024-10-06 07:44:46 +0800

    Clean the switchScene function of Sign up and Sign in Scenes

---
+++ src/main/java/application/pulselytics/controllers/SignInController.java
@@ -0,0 +1,25 @@
+package application.pulselytics.controllers;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import application.pulselytics.HelloApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SignInController {
+
+    @FXML
+    public void switchToSignUp(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(HelloApplication.class.getResource("scenes/SignUp.fxml")));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
