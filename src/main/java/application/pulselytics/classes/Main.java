dce5fbef25df54472bb7d947d83df9f7a010c68b
Jose Neil Silagan Jr. <neilsilagan@gmail.com>
2024-10-09 13:08:59 +0800

    Add static variable to store users
    Add admin user the the userStorage

--- src/main/java/application/pulselytics/classes/Main.java
+++ src/main/java/application/pulselytics/classes/Main.java
@@ -1,8 +1,28 @@
 package application.pulselytics.classes;
 
 
-public class Main {
-    public static void main (String[] args){
+import java.time.LocalDate;
+import java.util.HashMap;
 
+public class Main {
+    private static HashMap<String, User> userStorage = new HashMap<>();
+
+    public static HashMap<String, User> getUserStorage() {
+        return userStorage;
+    }
+
+    public static User getUser(String username){
+        return userStorage.get(username);
+    }
+
+    public static void addUser(User user){
+        userStorage.put(user.getUsername(), user);
+    }
+
+    public static void main (String[] args){
+        LocalDate birthday = LocalDate.parse("2005-03-27");
+        User admin = new User("Rj", "ArjTheProgrammer", "rjpogi123", "Male", birthday);
+
+        Main.addUser(admin);
     }
 }
