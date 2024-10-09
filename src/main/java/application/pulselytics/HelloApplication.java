bc80cdf187db80713082bd06f818d9d1a8ef9d68
Jose Neil Silagan Jr. <neilsilagan@gmail.com>
2024-10-10 07:54:44 +0800

    Initialize css file and add design on the Sign Up scene

--- src/main/java/application/pulselytics/HelloApplication.java
+++ src/main/java/application/pulselytics/HelloApplication.java
@@ -6,13 +6,15 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("scenes/Home.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("scenes/SignUp.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        scene.getStylesheets().add("styles.css");
+        stage.setTitle("Pulselytics");
         stage.setScene(scene);
         stage.show();
     }
