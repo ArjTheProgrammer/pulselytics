bddfc0a32cc74ce86b120aff58522e6b5379e3be
Jose Neil Silagan Jr. <neilsilagan@gmail.com>
2024-10-09 08:50:45 +0800

    Create a User class
    A User class that as a attribute of name, username, gender, birthday, and a password that all has getters and setters function besides the birthday attribute

---
+++ src/main/java/application/pulselytics/classes/BloodPressureLog.java
@@ -0,0 +1,2 @@
+package application.pulselytics.classes;public class BloodPressureLog {
+}
---
+++ src/main/java/application/pulselytics/classes/User.java
@@ -0,0 +1,79 @@
+package application.pulselytics.classes;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+public class User {
+    private String name;
+    private String username;
+    private String pass;
+    private String gender;
+    private LocalDate birthday;
+    private HashMap<LocalDateTime, BloodPressureLog> bloodPressureLogs;
+
+    public User(String name, String username, String pass, String gender, LocalDate birthday) {
+        this.name = name;
+        this.username = username;
+        this.pass = pass;
+        this.gender = gender;
+        this.birthday = birthday;
+        this.bloodPressureLogs = new HashMap<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public void setPass(String pass){
+        this.pass = pass;
+    }
+
+    public void setGender(String gender){
+        this.gender = gender;
+    }
+
+    public HashMap<LocalDateTime, BloodPressureLog> getBloodPressureLogs() {
+        return bloodPressureLogs;
+    }
+
+    public void addBloodPressureLog(BloodPressureLog log) {
+        bloodPressureLogs.put(log.getDateStamp(), log);
+    }
+
+    public void editBloodPressureLog(BloodPressureLog log) {
+        bloodPressureLogs.replace(log.getDateStamp(), log);
+    }
+
+    public void removeBloodPressureLog(BloodPressureLog log){
+        bloodPressureLogs.remove(log.getDateStamp());
+    }
+
+    public BloodPressureLog getBloodPressureLogByDate(LocalDateTime date) {
+        return bloodPressureLogs.get(date);
+    }
+}
