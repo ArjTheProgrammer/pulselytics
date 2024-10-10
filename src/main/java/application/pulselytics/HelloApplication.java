d4acbf723d8881539bb1d3e63daf73ba4c43fbb1
Jose Neil Silagan Jr. <neilsilagan@gmail.com>
2024-10-10 09:08:34 +0800

    switch to home when sign in is click in the sign in scene

--- src/main/java/application/pulselytics/HelloApplication.java
+++ src/main/java/application/pulselytics/HelloApplication.java
@@ -14,6 +14,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("scenes/SignUp.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
 
+        stage.setResizable(false);
         stage.setTitle("Pulselytics");
         stage.setScene(scene);
         stage.show();
--- src/main/java/application/pulselytics/controllers/SignInController.java
+++ src/main/java/application/pulselytics/controllers/SignInController.java
@@ -22,4 +22,12 @@ public class SignInController {
         stage.setScene(scene);
         stage.show();
     }
+
+    public void switchToHome(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(HelloApplication.class.getResource("scenes/Home.fxml")));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
