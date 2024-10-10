09d409468cb6427b1947b40796561c4a54afc535
Jose Neil Silagan Jr. <neilsilagan@gmail.com>
2024-10-10 08:05:37 +0800

    Change the approach and inject the css file to the fxml file. Add design in the sign in scene

--- src/main/java/application/pulselytics/HelloApplication.java
+++ src/main/java/application/pulselytics/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("scenes/SignUp.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        scene.getStylesheets().add("styles.css");
+
         stage.setTitle("Pulselytics");
         stage.setScene(scene);
         stage.show();
