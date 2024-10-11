106f7bc17b6c09dbafdbfe865cdf7bc2b0595178
Jose Neil Silagan Jr. <neilsilagan@gmail.com>
2024-10-12 07:36:14 +0800

    Create a bpTypeIdentifier in another class called Tools
    It identifies what type of blood pressure base on the systolic and diastolic input (Hypotension, Normal, Elevated, Hypertension Stage 1, Hypertension Stage 2, and Hypertensive Crisis)

--- src/main/java/application/pulselytics/classes/Tools.java
+++ src/main/java/application/pulselytics/classes/Tools.java
@@ -1,2 +1,34 @@
-package application.pulselytics.classes;public class Tools {
+package application.pulselytics.classes;
+
+import java.util.Scanner;
+
+public class Tools {
+    public static String bpTypeIdentifier (int systolic, int diastolic){
+        if (systolic < 90 && diastolic < 60)
+        {
+            return "Hypotension";
+        }
+        else if (systolic < 120 && diastolic < 80)
+        {
+            return "Normal";
+        }
+        else if (systolic >= 120 && systolic < 130 && diastolic < 80)
+        {
+            return "Elevated";
+        }
+        else if ((systolic >= 130 && systolic < 140) || (diastolic >= 80 && diastolic < 90))
+        {
+            return "Hypertension Stage 1";
+        }
+        else if ((systolic >= 140 && systolic < 180) || (diastolic >= 90 && diastolic < 120)) {
+            return "Hypertension Stage 2";
+        }
+        else if ((systolic > 180 && diastolic > 120)) {
+            return "Hypertensive Crisis";
+        }
+        else
+        {
+            return "Invalid Input";
+        }
+    }
 }
