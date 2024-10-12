ea2306ac89b07b715120701443cd7668760e3b90
Jose Neil Silagan Jr. <neilsilagan@gmail.com>
2024-10-12 15:09:55 +0800

    Add ID to the controllers of SignUp and initialize the gender combobox

--- src/main/java/application/pulselytics/controllers/SignUpController.java
+++ src/main/java/application/pulselytics/controllers/SignUpController.java
@@ -1,18 +1,44 @@
 package application.pulselytics.controllers;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
 import application.pulselytics.HelloApplication;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class SignUpController {
+public class SignUpController implements Initializable {
+    @FXML
+    private DatePicker signUpBirthday;
+
+    @FXML
+    private ComboBox<String> signUpGender;
+
+    @FXML
+    private TextField signUpName;
+
+    @FXML
+    private PasswordField signUpPassword;
+
+    @FXML
+    private TextField signUpUsername;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        signUpGender.getItems().addAll("Male", "Female", "LGBTQIA+");
+    }
 
     @FXML
     public void switchToSignIn(ActionEvent event) throws IOException {
