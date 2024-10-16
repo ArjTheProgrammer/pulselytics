55fa82c88c5f7442d10466622034bb3cb6930742
Jose Neil Silagan Jr. <neilsilagan@gmail.com>
2024-10-16 11:28:07 +0800

    Display the logs in the LogLayout using the logCard

--- src/main/java/application/pulselytics/controllers/HistoryController.java
+++ src/main/java/application/pulselytics/controllers/HistoryController.java
@@ -1,22 +1,55 @@
 package application.pulselytics.controllers;
 
 import application.pulselytics.HelloApplication;
+import application.pulselytics.classes.BloodPressureLog;
+import application.pulselytics.classes.Main;
+import application.pulselytics.classes.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class HistoryController {
+public class HistoryController implements Initializable {
 
     @FXML
     private VBox settingsBox;
+
+    @FXML
+    private VBox logLayout;
+
+    private final User currentUser = Main.getCurrentUser();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        HashMap<LocalDateTime, BloodPressureLog> userBloodPressureLogs = currentUser.getBloodPressureLogs();
+
+        try {
+            for (LocalDateTime datestamp : userBloodPressureLogs.keySet()){
+                FXMLLoader  fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(HelloApplication.class.getResource("scenes/LogCard.fxml"));
+                HBox logCard = fxmlLoader.load();
+                LogCardController logCardController = fxmlLoader.getController();
+                logCardController.setData(currentUser, userBloodPressureLogs.get(datestamp));
+                logLayout.getChildren().add(logCard);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     @FXML
     public void switchToHome(ActionEvent event) throws IOException {
--- src/main/java/application/pulselytics/controllers/LogCardController.java
+++ src/main/java/application/pulselytics/controllers/LogCardController.java
@@ -1,4 +1,57 @@
 package application.pulselytics.controllers;
 
+import application.pulselytics.classes.BloodPressureLog;
+import application.pulselytics.classes.User;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.time.format.DateTimeFormatter;
+
 public class LogCardController {
+//    @FXML
+//    private VBox logLayout = HistoryController;
+
+    @FXML
+    private HBox cardBloodPressure;
+
+    @FXML
+    private Label cardDate;
+
+    @FXML
+    private Label cardTime;
+
+    @FXML
+    private Button cardDelete;
+
+    @FXML
+    private Label cardDiastolic;
+
+    @FXML
+    private Label cardSystolic;
+
+    @FXML
+    private Label cardType;
+
+
+    public void setData(User user,BloodPressureLog bloodPressureLog){
+
+        cardDate.setText(bloodPressureLog.getDateStamp().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        cardTime.setText(bloodPressureLog.getDateStamp().format(DateTimeFormatter.ofPattern("HH:mm")));
+        cardSystolic.setText(Integer.toString(bloodPressureLog.getSystolic()));
+        cardDiastolic.setText(Integer.toString(bloodPressureLog.getDiastolic()));
+        cardType.setText(bloodPressureLog.getType());
+
+//        cardDelete.setOnAction(event -> {
+//            handleDelete(bloodPressureLog, user, cardBloodPressure);
+//        });
+    }
+
+//    private void handleDelete(BloodPressureLog bloodPressureLog, User user, HBox card){
+//        user.removeBloodPressureLog(bloodPressureLog);
+//
+////        HistoryController.logLayout.getChildren().remove(card);
+//    }
 }
