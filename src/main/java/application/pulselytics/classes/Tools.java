aa70fad221b61d1db93428ddcd7cc93a2da6801b
Jose Neil Silagan Jr. <neilsilagan@gmail.com>
2024-10-18 14:51:39 +0800

    Adjust the bpIdentifier that checks the sys and dia from highest to lowest not lowest to highest
    Fixed this bug because it doesn't reach the hypertensive crisis because it doesn't proceed from hypertension stage 2

--- src/main/java/application/pulselytics/classes/Tools.java
+++ src/main/java/application/pulselytics/classes/Tools.java
@@ -6,27 +6,18 @@ import javafx.scene.control.Label;
 
 public class Tools {
     public static String bpTypeIdentifier (int systolic, int diastolic){
-        if (systolic < 90 && diastolic < 60)
-        {
-            return "Hypotension";
-        }
-        else if (systolic < 120 && diastolic < 80)
-        {
-            return "Normal";
-        }
-        else if (systolic >= 120 && systolic < 130 && diastolic < 80)
-        {
-            return "Elevated";
-        }
-        else if ((systolic >= 130 && systolic < 140) || (diastolic >= 80 && diastolic < 90))
-        {
-            return "Hypertension Stage 1";
-        }
-        else if ((systolic >= 140 && systolic < 180) || (diastolic >= 90 && diastolic < 120)) {
-            return "Hypertension Stage 2";
-        }
-        else {
+        if (systolic >= 180 || diastolic >= 120) {
             return "Hypertensive Crisis";
+        } else if (systolic >= 140 || diastolic >= 90) {
+            return "Hypertension Stage 2";
+        } else if (systolic >= 130 || diastolic > 80) {
+            return "Hypertension Stage 1";
+        } else if (systolic >= 120 && diastolic < 80) {
+            return "Elevated";
+        } else if (systolic >= 90 && diastolic < 80) {
+            return "Normal";
+        } else {
+            return "Hypotension";
         }
     }
 
